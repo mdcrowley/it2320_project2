216e534e3ffa01aa8517d741a85de4366ba416ef
mdcrowley <matthew.crowley@tri-c.edu>
2024-04-11 00:04:20 -0400

    Update project.js
    Removing unneeded code

--- project.js
+++ project.js
@@ -1,12 +1,1 @@
 
-function salesByPerson() {
-   alert("Display data in this alert box.");
-}
-
-function bestAndWorst() {
-   alert("Display data in this alert box.");
-}
-
-function commissionsByMonth() {
-   alert("Display data in this alert box.");
-}
